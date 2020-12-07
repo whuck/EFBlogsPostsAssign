f32a5dbc92b01a9c1250c10033cac76dcb0debab
whuck <70232597+whuck@users.noreply.github.com>
2020-12-07 14:44:34 -0600

    donezo, cleanedup validation added

--- Blog.cs
+++ Blog.cs
@@ -1,12 +1,12 @@
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+
 namespace BlogsConsole
 {
         public class Blog
     {
         public int BlogId { get; set; }
         public string Name { get; set; }
-        [Required(ErrorMessage = "Blog Name cannot be null")]
+
         public List<Post> Posts { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using NLog.Web;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlogsConsole
@@ -11,18 +10,6 @@ namespace BlogsConsole
     class Program
     {
 
-        //nlog dotnet add package NLog.Web.AspNetCore
-        //dotnet add package Microsoft.EntityFrameworkCore.SqlServer
-        //dotnet add package Microsoft.EntityFrameworkCore.Tools
-//         // create static instance of Logger
-//
-//creates migrations folder an dclasses to do data migration
-// - dotnet ef migrations add CreateDatabase
-// - dotnet ef database update
-//dotnet add package Microsoft.Extension.Configuration
-
-//dotnet tool update --global dotnet-ef
-// only have to do that once.
         private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
         static void Main(string[] args)
         {
@@ -49,47 +36,39 @@ namespace BlogsConsole
 
                         Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine($"{query.Count()} blog records returned");
-                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.ForegroundColor = ConsoleColor.White;
                         foreach (var item in query)
                         {
                             Console.WriteLine($"{item.BlogId} - {item.Name}");
-                            // foreach (var post in item.Posts) {
-                            //     Console.WriteLine($"Post:{post.PostId}-{post.Title}::{post.Content}");
-                            // }
                         }
-                        Console.ForegroundColor = ConsoleColor.White;
+                        
                     }
                     else if (choice == "2") //add blog
                     {
+                        //make blog obj, grab input for blog.Name
                         Blog blog = new Blog();
                         Console.WriteLine("Enter a name for a new Blog:");
                         blog.Name = Console.ReadLine();
-
-                        ValidationContext context = new ValidationContext(blog, null, null);
-                        List<ValidationResult> results = new List<ValidationResult>();
-
-                        var isValid = Validator.TryValidateObject(blog, context, results, true);
+                        var isValid = false;
+                        //if name is null, die honorably
+                        if (blog.Name == "") {
+                            logger.Error("Blog Name cannot be null!");
+                        } else {
+                            isValid = true;
+                        }
                         if (isValid)
                         {
                             var db = new BloggingContext();
                             // check for unique name
                             if (db.Blogs.Any(c => c.Name == blog.Name))
                             {
-                                // generate validation error
                                 isValid = false;
-                                results.Add(new ValidationResult("Name exists", new string[] { "Name" }));
+                                logger.Error("Name exists");
                             }
                             else
                             {
-                                logger.Info("Validation passed");
                                 db.AddBlog(blog);
-                            }
-                        }
-                        if (!isValid)
-                        {
-                            foreach (var result in results)
-                            {
-                                logger.Error($"{result.MemberNames.First()} : {result.ErrorMessage}");
+                                logger.Info($"Blog added - {blog.Name}");
                             }
                         }
                     }
@@ -100,44 +79,42 @@ namespace BlogsConsole
                         var query = db.Blogs.OrderBy(p => p.BlogId);
 
                         Console.WriteLine("Enter the id of a Blog to add a Post:");
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
                         foreach (var b in query)
                         {
                             Console.WriteLine($"[{b.BlogId}] {b.Name}");
                         }
-                        Console.ForegroundColor = ConsoleColor.White;
                         
-                        int id = int.Parse(Console.ReadLine());
-
-                        //error id is not a number
-                        //error id does not exist in db
-                        //Console.Clear();
-                        logger.Info($"Blog {id} selected");
-                //enter post title
-                        Console.WriteLine("Enter the Post title:");
-                        var t = Console.ReadLine();
-                //error title cannot be null
-                //enter post content- can be null
-                        Console.WriteLine("Enter the Post Content:");
-                        var c = Console.ReadLine();
-
-                        var post = new Post();
-                        post.Title = t;
-                        post.Content = c;
-                        post.BlogId = id;
-                        db.AddPost(post);
-
-                        //lazy loading, will not populate the category.Products list
-                        //Category category = db.Categories.FirstOrDefault(c => c.CategoryId == id);
-                        
-                        //eager loading, will load Products list
-                        //Category category = db.Categories.Include("Products").FirstOrDefault(c => c.CategoryId == id);
-                        
-                        // Console.WriteLine($"{blog.CategoryName} - {blog.Description}");
-                        // foreach (Post p in blog.Posts)
-                        // {
-                        //     Console.WriteLine(p.ProductName);
-                        // }                                                
+                        try {
+                            //this'll throw a FormatException if an int wasn't entered
+                            int id = int.Parse(Console.ReadLine());
+                            //check for valid id
+                            if (!db.Blogs.Any(c => c.BlogId == id)) {
+                                logger.Error("There are no Blogs saved with that Id");
+                            } else {
+                                logger.Info($"Blog {id} selected");
+                                
+                                //enter post title
+                                Console.WriteLine("Enter the Post title:");
+                                var t = Console.ReadLine();
+                                //null title check
+                                if ( t == "" ) {
+                                    logger.Error("Post title cannot be null");
+                                } else {
+                                    Console.WriteLine("Enter the Post Content:");
+                                    var c = Console.ReadLine();
+                                    var post = new Post();
+                                    post.Title = t;
+                                    post.Content = c;
+                                    post.BlogId = id;
+                                    db.AddPost(post);
+                                    logger.Info($"Post added - {post.Title}");
+                                }
+                            }
+                        } catch (System.FormatException) {
+                            logger.Error($"Invalid Blog Id");
+                        } catch (Exception e ) {
+                            logger.Error(e.GetType());
+                        }                                           
                     }                                        
                     else if (choice == "4") //display posts
                     {   
@@ -145,42 +122,38 @@ namespace BlogsConsole
                         Console.WriteLine("0) Posts from all blogs");
                         var db = new BloggingContext();
                         var getBlogsquery = db.Blogs.OrderBy(p=>p.BlogId);
+                        //get blog names
                         foreach(var b in getBlogsquery) {
                             Console.WriteLine($"{b.BlogId}) Posts from {b.Name}");
                         }
-                        
-                        int id = int.Parse(Console.ReadLine());
-                        
-                        var query = db.Blogs.Where(b => b.BlogId == id).Include("Posts").OrderBy(p => p.BlogId);
-                        foreach (var item in query)
-                        {
-                            Console.WriteLine($"{item.Name}");
-                            foreach (Post p in item.Posts)
-                            {
-                                Console.WriteLine($"\t{p.Title}::{p.Content}");
+                        try { // throws format exception if input isnt an int
+                            int id = int.Parse(Console.ReadLine());
+                            IEnumerable<Blog> query;
+                            if (id != 0) {
+                                query = db.Blogs.Where(b => b.BlogId == id).Include("Posts").OrderBy(p => p.BlogId);
+                            } else {
+                                query = db.Blogs.Include("Posts").OrderBy(p => p.BlogId);
                             }
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"{query.Count()} blog records returned");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            foreach (var item in query)
+                            {
+                                Console.WriteLine($"Blog: {item.Name} - Posts: {item.Posts.Count}");
+                                foreach (Post p in item.Posts)
+                                {
+                                    Console.WriteLine($"\tTitle: {p.Title}");
+                                    Console.WriteLine($"\tContent: {p.Content}");
+                                }
+                            }
+                            } catch (System.FormatException) {
+                                logger.Error("Please enter 0 or a valid blog id");
                         }
+                       
                     }
                     Console.WriteLine();
+                    
                 } while (choice.ToLower() != "q");
-                // // Create and save a new Blog
-                // Console.Write("Enter a name for a new Blog: ");
-                // var name = Console.ReadLine();
-
-                // var blog = new Blog { Name = name };
-
-                // var db = new BloggingContext();
-                // db.AddBlog(blog);
-                // logger.Info("Blog added - {name}", name);
-
-                // // Display all Blogs from the database
-                // var query = db.Blogs.OrderBy(b => b.Name);
-
-                // Console.WriteLine("All blogs in the database:");
-                // foreach (var item in query)
-                // {
-                //     Console.WriteLine(item.Name);
-                // }
             }
             catch (Exception ex)
             {
